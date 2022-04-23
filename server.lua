de4592c5723cb2077b361115917a37767107444f
Furkan <83224389+FuurkanOzturk@users.noreply.github.com>
2022-04-23 14:48:09 +0300

    Add files via upload

---
+++ server.lua
@@ -0,0 +1,14 @@
+﻿function enterthecar(thePlayer, seat, jacked)
+	if (seat == 0) then
+		local playerVehicle = getPedOccupiedVehicle(thePlayer)
+		if playerVehicle and isElement(playerVehicle) and getElementModel(playerVehicle) == 490 and isControlEnabled(thePlayer, "horn") then
+			toggleControl(thePlayer, "horn", false)
+		end
+		local accName = getAccountName(getPlayerAccount(thePlayer)) -- get his account name
+		if isObjectInACLGroup ("user."..accName, aclGetGroup("Admin")) then -- Does he have access to Admin functions?
+			toggleControl(thePlayer, "horn", true)
+			outputChatBox("[!] #ffffffYou can use the horn in the car..", thePlayer, 0, 0, 0, true)
+		end
+	end
+end
+addEventHandler("onVehicleEnter", getRootElement(), enterthecar)
